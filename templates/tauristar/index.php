2a05a2a6e6171a26631aad764c82a8ab375aa21b
laoneo <allon.moritz@digital-peak.com>
2016-07-01 12:48:47 +0200

    Adapt template to bs3

--- templates/tauristar/index.php
+++ templates/tauristar/index.php
@@ -47,67 +47,33 @@ switch ($params->get('mode', 2))
 				<jdoc:include type="modules" name="logo" style="none" />
 			</a>
 
-			<nav class="navbar navbar-default navbar-fixed-top">
-		      <div class="container">
-		        <div class="navbar-header">
-		          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-		            <span class="sr-only">Toggle navigation</span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-		          </button>
-		          <a class="navbar-brand" href="#">Project name</a>
-		        </div>
-		        <div id="navbar" class="navbar-collapse collapse">
-		          <ul class="nav navbar-nav">
-		            <li class="active"><a href="#">Home</a></li>
-		            <li><a href="#about">About</a></li>
-		          </ul>
-		        </div>
-		        <div id="navbar-menu" class="navbar-collapse collapse navbar-right">
-	            	<jdoc:include type="modules" name="menu" style="none" /></div>
-		        </div>
-		      </div>
-		    </nav>
-
-			<div class="pull-right">
-				<div class="pull-right" id="login-button">
-					<?php if ($user->guest)
-					{?>
-						<form action="<?php echo JRoute::_('index.php?option=com_users&view=login', true, true)?>" method="post" class="pull-right link-button">
-							<input type="hidden" name="return" value="<?php echo base64_encode(JURI::getInstance(JFactory::getURI()->toString(array('path', 'query', 'fragment'))));?>"/>
-							<input type="submit" class="btn btn-inverse pull-right link-button" value="<?php echo JText::_('JLOGIN');?>"/>
-						</form>
-						<?php
-						$usersConfig = JComponentHelper::getParams('com_users');
-						if ($usersConfig->get('allowUserRegistration'))
-						{?>
-							<form action="<?php echo JRoute::_('index.php?option=com_users&view=registration', true, true)?>" method="post" class="pull-right link-button">
-								<input type="hidden" name="return" value="<?php echo base64_encode(JURI::getInstance(JFactory::getURI()->toString(array('path', 'query', 'fragment'))));?>"/>
-								<input type="submit" class="btn btn-inverse pull-right link-button" value="<?php echo JText::_('TPL_TAURISTAR_SIGN_UP'); ?>"/>
-							</form>
-						<?php
-						}
-					}
-					else
-					{ ?>
-						<a class="btn btn-inverse btn-mini link-button" href="<?php echo JRoute::_('index.php?option=com_users&task=user.logout&return=Lw&'.JSession::getFormToken().'=1')?>">Logout</a>
-					<?php
-					} ?>
+			<nav class="navbar navbar-default">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Project name</a>
+					</div>
+					<div id="navbar-menu" class="navbar-collapse collapse">
+						<jdoc:include type="modules" name="menu" style="none" />
+					</div>
 				</div>
-			</div>
+		    </nav>
 		</div>
 	</div>
 
     <?php
     if ($this->countModules('top'))
     { ?>
-    <div id="top">
-        <div class="container">
-			<div class="row-fluid">
+	<div id="top">
+		<div class="container">
+			<div class="row">
 				<jdoc:include type="modules" name="top" style="xhtml" />
 			</div>
-            <div class="clr"></div>
 		</div>
 	</div>
     <hr class="soften">
@@ -115,7 +81,7 @@ switch ($params->get('mode', 2))
     } ?>
     <div id="bodysection">
     	<div class="container">
-      		<div class="row-fluid">
+      		<div class="row">
             	<?php
             	$left = $this->countModules('sidebar-left');
             	$right = $this->countModules('sidebar-right');
@@ -124,20 +90,18 @@ switch ($params->get('mode', 2))
             	$contentSpan = $left ? $contentSpan - $sidebarWidth : $contentSpan;
             	if ($left)
             	{ ?>
-                <div class="span<?php echo $sidebarWidth?>">
+                <div class="col-md-<?php echo $sidebarWidth?>">
                 	<jdoc:include type="modules" name="sidebar-left" style="xhtml" />
                 </div>
                 <?php
             	} ?>
-                <div class="content span<?php echo $contentSpan?>">
-                	<div class="row-fluid">
-                		<jdoc:include type="message" />
-                		<jdoc:include type="component" />
-                	</div>
+                <div class="content col-md-<?php echo $contentSpan?>">
+                	<jdoc:include type="message" />
+                	<jdoc:include type="component" />
                 </div>
                 <?php if ($right)
                 { ?>
-                <div class="span<?php echo $sidebarWidth?>">
+                <div class="col-md-<?php echo $sidebarWidth?>">
                 	<jdoc:include type="modules" name="sidebar-right" style="xhtml" />
                 </div>
                 <?php
@@ -150,11 +114,7 @@ switch ($params->get('mode', 2))
     { ?>
 	<div id="breadcrumbs">
 	    <div class="container">
-		  <div class="row-fluid">
-		    <div class="span12">
-		      <jdoc:include type="modules" name="breadcrumbs" />
-		    </div>
-		  </div>
+			<jdoc:include type="modules" name="breadcrumbs" />
 		</div>
 	</div>
     <?php
@@ -163,24 +123,19 @@ switch ($params->get('mode', 2))
     { ?>
     <div id="bottom">
         <div class="container">
-			<div class="row-fluid">
-				<jdoc:include type="modules" name="bottom" style="html5"/>
-			</div>
-            <div class="clr"></div>
+			<jdoc:include type="modules" name="bottom" style="html5"/>
 		</div>
 	</div>
     <?php
     } ?>
     <footer id="footer">
     	<div class="container">
-	    <?php if ($this->countModules('footer'))
-	    { ?>
-			<div class="row-fluid">
+		    <?php if ($this->countModules('footer'))
+		    { ?>
 				<jdoc:include type="modules" name="footer" style="xhtml" />
-			</div>
-	    <?php
-	    } ?>
-		<p>Copyright &copy; <?php echo date('Y'); ?> - <?php echo JFactory::getApplication()->getCfg('sitename'); ?></p>
+		    <?php
+		    } ?>
+			<p>Copyright &copy; <?php echo date('Y'); ?> - <?php echo JFactory::getApplication()->getCfg('sitename'); ?></p>
 		</div>
     </footer>
 </body>
